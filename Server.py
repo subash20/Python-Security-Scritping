42b0f63784a5f27119b9b570999395ea9aea44f1
subash20 <medawadisubash@gmail.com>
2016-10-21 13:46:30 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ import socket
 
 #tcp connection
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)  # connection to same port
 s.bind(('0.0.0.0',8000))
 s.listen(3)
 (client, (ip, port))=s.accept()
